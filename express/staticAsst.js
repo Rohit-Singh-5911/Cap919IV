30ebd59e55c9f5aedda5cccfde1ac973255d7891
Rohit Singh Shekhawat <117024078+Rohit-Singh-5911@users.noreply.github.com>
2024-03-28 10:26:15 +0530

    Add files via upload

---
+++ express/staticAsst.js
@@ -0,0 +1,8 @@
+const path = require('path')
+const express = require('express')
+console.log("Output Below");
+console.log(__dirname)
+console.log(__filename)
+console.log(path.join(__dirname,'..'))
+console.log(path.join(__dirname,'../..'))
+console.log(path.join(__dirname,'../public'))
